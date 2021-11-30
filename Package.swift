9de26e2d308e210a26ffc32e7517c0e3cd299a6b
Vitalii <zadubniy.v@gmail.com>
2021-11-30 12:34:53 +0200

    change package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "GlobalEMobileSDK",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,17 +13,12 @@ let package = Package(
             targets: ["GlobalEMobileSDK"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(url: "https://github.com/Adyen/adyen-ios", from: "3.8.5"),
+         .package(name: "Braintree", url: "https://github.com/braintree/braintree_ios", branch: "master"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "GlobalEMobileSDK",
-            dependencies: []),
-        .testTarget(
-            name: "GlobalEMobileSDKTests",
-            dependencies: ["GlobalEMobileSDK"]),
+        .binaryTarget(name: "GlobalEMobileSDK",
+                      url: "https://github.com/Zadubnyi/GlobalEMobileSDK/blob/main/GlobalEMobileSDK.xcframework.zip",
+                      checksum: "68675d9d5d2797fd9a0bb5d3291f9daf26eb30915c60c09b846af9b9008d4252")
     ]
 )
